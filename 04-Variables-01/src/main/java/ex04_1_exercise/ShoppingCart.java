362030bbbe6e318329075bc9e01b2de8ef96b238
Ivan Garcia <isc.ivgarcia@gmail.com>
2019-09-07 14:13:10 -0500

    Excercise 04

--- 04-Variables-01/src/main/java/ex04_1_exercise/ShoppingCart.java
+++ 04-Variables-01/src/main/java/ex04_1_exercise/ShoppingCart.java
@@ -2,21 +2,29 @@
 package ex04_1_exercise;
 
 public class ShoppingCart {
-    
-    public static void main(String[] args) {
-         // Declare and initialize String variables.  Do not initialize message yet.
-        
-        
- 
- 
-        
-        
-        // Assign the message variable 
 
-        
-        // Print and run the code
-        
+	public static void main(String[] args) {
+		// Declare and initialize String variables. Do not initialize message yet.
+		String customerName = "Ivan Garcia";
+		String itemDesc = "Shirt";
 
-    }
-    
+		// message declaration
+		String message;
+
+		// Assign the message variable
+		message = customerName + " wants to purchase a " + itemDesc;
+
+		// Print and run the code
+		System.out.println(message);
+
+		// Assign the message variable
+		message = message + " !!!";
+
+		// Print and run the code
+		System.out.println(message);
+		
+		System.out.println(3/0);
+
+	}
+
 }
--- 04-Variables-02/src/main/java/ex04_2_exercise/ShoppingCart.java
+++ 04-Variables-02/src/main/java/ex04_2_exercise/ShoppingCart.java
@@ -2,23 +2,32 @@
 package ex04_2_exercise;
 
 public class ShoppingCart {
-    
-    public static void main(String[] args) {
-        String custName = "Mary Smith";
-        String itemDesc = "Shirt";
-        String message = custName+" wants to purchase a "+itemDesc;
-        
-        // Declare and initialize numeric fields: price, tax, quantity, and total.   
-        
-        
-        // Modify message to include quantity 
-        
-        
-        System.out.println(message);
-        
-        // Calculate total and then print the total cost
-        
 
-        
-    }    
+	public static void main(String[] args) {
+		String customerName = "Ivan Garcia";
+		String itemDesc = "Shirt";
+		
+		String message = customerName + " wants to purchase a " + itemDesc;
+
+		System.out.println(message);
+
+		// Declare and initialize numeric fields: price, tax, quantity, and total.
+		double price, tax;
+		price = 29.99;
+		tax = 1.04;
+
+		int quantity = 2;
+
+		// Modify message to include quantity
+		message = customerName + " wants to purchase " + quantity + " " + itemDesc;
+
+		System.out.println(message);
+
+		// Calculate total and then print the total cost
+
+		double total = quantity * price * tax;
+
+		System.out.println("Total cost with taxes: " + total);
+
+	}
 }
